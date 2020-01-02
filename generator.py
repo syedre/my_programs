a23936a9241c7cc19c54524e62029101248c195d
syedre <46558740+syedre@users.noreply.github.com>
2020-01-02 12:45:14 +0530

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,15 @@
+def gene():
+    for i in range(5):
+        yield(i*i)
+
+obj = gene()
+# you  can call one by one
+print(next(obj))
+
+# Another way to create generator is as follows
+
+obj = (i*i for i in range(5))
+print(next(obj))
+# you can loop through  generator object instead of typing next everytime
+for i in obj:
+    print(i)
