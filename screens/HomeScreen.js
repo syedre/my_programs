e22fde5c05aba224920876d8139c4c1eb9ea826d
syedre <46558740+syedre@users.noreply.github.com>
2020-01-29 11:56:35 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View,StyleSheet } from 'react-native';
+import {Header,Left,Right,Icon} from 'native-base'
 
 class HomeScreen extends Component {
   render() {
     return (
       <View style= {styles.container} >
-        <Text>Hello, world!</Text>
+        <Header style ={styles.header}>
+          <Left>
+            <Icon name = "menu" onPress= {()=>this.props.navigation.openDrawer()} />
+          </Left>
+        </Header>
+        <View style ={{flex:1,alignItems:'center',justifyContent:'center'}}>
+        <Text> Hello  </Text>
+        <Icon name="home" />
+        <Text> Home </Text>
+        </View>
       </View>
     );
   }
@@ -15,11 +25,18 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-  
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    alignItems:'left',
+    flexDirection:'row',
+    
+    
+  },
+  header :{
+    flexDirection:'row',
+    backgroundColor:'white',
+    padding:10,
+
   }
 
   });
+
 
