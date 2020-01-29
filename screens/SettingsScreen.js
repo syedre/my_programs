57f6975dcd7f280907794aa5e0f69ed9d750728f
syedre <46558740+syedre@users.noreply.github.com>
2020-01-29 11:29:32 +0530

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Text, View,StyleSheet } from 'react-native';
+import Compo from './compo'
+import Search from './search'
+import {Header,Left,Right,Icon} from 'native-base'
 
 class SettingsScreen extends Component {
   render() {
     return (
       <View style= {styles.container} >
-        <Text>Hello, world!</Text>
+        <Header style ={styles.header}>
+          <Left>
+            <Icon name = "menu" onPress= {()=>this.props.navigation.openDrawer()} />
+          </Left>
+        </Header>
+       <Compo/>
+        
+        
       </View>
     );
   }
@@ -15,11 +25,20 @@ export default SettingsScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    alignItems:'left',
+    flexDirection:'row',
+    //flex: 1,
+    //justifyContent: 'center',
   
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  }
+    //backgroundColor: '#ecf0f1',
+    //padding: 8,
+
+  },
+  header:{
+    flexDirection:'row',
+    backgroundColor:'white',
+    padding:2
+
+  },
 
   });
-
